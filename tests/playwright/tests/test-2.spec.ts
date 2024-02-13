3d84d7f625f47d0c6b87c3c8e8e0a83ebb5e291b
PratyaPothong <pratya.po@kkumail.com>
2024-02-13 23:52:01 +0700

    edit test-2.spec.ts

--- tests/playwright/tests/test-2.spec.ts
+++ tests/playwright/tests/test-2.spec.ts
@@ -6,9 +6,9 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: '' }).click();
   await page.getByRole('button', { name: ' Add to cart' }).click();
   await page.getByRole('link', { name: ' Proceed to checkout' }).click();
-  await page.waitForTimeout(1250);
+  await page.waitForTimeout(750);
   await page.getByRole('link', { name: 'Proceed to checkout' }).click();
-  await page.waitForTimeout(1250);
+  await page.waitForTimeout(750);
   await page.getByLabel('Mr.').check();
   await page.getByLabel('First name').click();
   await page.getByLabel('First name').fill('Guest');
@@ -16,18 +16,18 @@ test('test', async ({ page }) => {
   await page.getByLabel('Last name').fill('Guest');
   await page.getByLabel('Email').click();
   await page.getByLabel('Email').fill('pratya.po@kkumail.com');
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
   await page.getByRole('button', { name: 'Continue' }).click();
-  await page.waitForTimeout(1250);
+  await page.waitForTimeout(1000);
   await page.getByText('I agree to the terms of').click();
-  await page.waitForTimeout(1250);
+  await page.waitForTimeout(1000);
   await page.getByRole('button', { name: 'Place order' }).click();
-  await page.waitForTimeout(1250);
-  await page.getByLabel('QR Payment').check();
-  await page.waitForTimeout(2000);
-  await page.getByLabel('แนบสลิปการโอน').click();
-  await page.getByLabel('แนบสลิปการโอน').setInputFiles('QR_payment_test.PNG');
+  await page.waitForTimeout(1000);
+
+  await page.locator('label').filter({ hasText: 'Choose file' }).click();
+  await page.getByLabel('Choose file').setInputFiles('QR_payment_test.PNG');
   await page.getByRole('button', { name: 'Upload' }).click();
+
   await page.click('body');
   await page.waitForTimeout(20000);
 });
