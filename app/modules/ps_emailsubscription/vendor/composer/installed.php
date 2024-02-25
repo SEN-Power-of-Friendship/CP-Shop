553f05911d733b9d836de61e50b0b36515e8951f
GURESPEX <natthanan12318@gmail.com>
2024-02-25 15:15:47 +0700

    Upgrade build in Module

---
+++ app/modules/ps_emailsubscription/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/ps_emailsubscription',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9fe6e3351a5bb1e1c964e004daea1d387b777065',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_emailsubscription' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9fe6e3351a5bb1e1c964e004daea1d387b777065',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ app/modules/ps_googleanalytics/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/ps_googleanalytics',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9f18ecee06bc62da8ff21553be2d46602a30e3e5',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_googleanalytics' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9f18ecee06bc62da8ff21553be2d46602a30e3e5',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
